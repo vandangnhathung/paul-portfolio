c97933388fc33564fdb22f56833771adf898b84e
vandangnhathung <vandangnhathung@gmail.com>
2025-12-02 01:08:26 +0700

    feat: add fading-transition (not completed yet

--- registry/paul/blocks/fading-transition/example.tsx
+++ registry/paul/blocks/fading-transition/example.tsx
@@ -1,12 +1,39 @@
-// This is a template file - replace 'component-name' with your actual component name
-// The import path will be updated when you use the create script
-// import { ComponentName } from "@/registry/paul/blocks/component-name/component-name"
+import {FadingTransition} from "@/registry/paul/blocks/fading-transition/fading-transition";
 
 export default function Example() {
-  return (
-    <div className="container mx-auto p-8">
-      {/* <ComponentName exampleProp="Example value" /> */}
-    </div>
-  )
+    const images = [
+        {
+            url: "https://images.unsplash.com/photo-1727341554370-80e0fe9ad082?q=80&w=2276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "A Black and White Photo of a Woman Brushing Her Teeth",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1640680608781-2e4199dd1579?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "Neon Palm",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1726083085160-feeb4e1e5b00?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "A blurry photo of a crowd of people",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1562016600-ece13e8ba570?q=80&w=2838&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "Rippling Crystal Blue Water",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1624344965199-ed40391d20f2?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "Mann im schwarzen Hemd unter blauem Himmel",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1689553079282-45df1b35741b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "A women with a flower crown on her head",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1721968317938-cf8c60fccd1a?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "A blurry photo of white flowers in a field",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1677338354108-223e807fb1bd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "A table topped with two wine glasses and plates",
+        },
+    ]
+    return <FadingTransition images={images}/>
 }
-
--- registry/paul/blocks/fading-transition/fading-transition.tsx
+++ registry/paul/blocks/fading-transition/fading-transition.tsx
@@ -1,30 +1,122 @@
-"use client"
+"use client";
 
-import React from "react"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+import React from "react";
 
 /**
  * Component description goes here.
  * This is a reusable component template for the registry system.
- * 
+ *
  * @see https://your-api-docs-url.com for API documentation
  */
-export interface ComponentNameProps {
-  /** Description of the prop */
-  exampleProp?: string
+export interface FadingTransitionProps {
+  /** Array of images. **/
+  images?: { url: string; title: string }[];
 }
 
 /**
- * ComponentName - Brief description of what this component does
- * 
+ * FadingTransition - Brief description of what this component does
+ *
  * @param props - Component props
  * @returns JSX element
  */
-export function ComponentName({ exampleProp = "default value" }: ComponentNameProps) {
-  return (
-    <div className="p-4">
-      <h1>Component Name</h1>
-      <p>{exampleProp}</p>
-    </div>
-  )
-}
+export function FadingTransition({ images }: FadingTransitionProps) {
+  useGSAP(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+      lerp: 0.05,
+    });
+    lenis.start();
 
+    gsap.registerPlugin(ScrollTrigger);
+    const pinHeight = document.querySelector(".pin-height") as HTMLElement;
+    const slider = document.querySelector(".slider") as HTMLElement;
+    const medias = document.querySelectorAll(".slide-main-img");
+
+
+    ScrollTrigger.create({
+      trigger: pinHeight,
+      start: 'top top',
+      end: 'bottom bottom',
+      pin: slider
+  })
+
+      gsap.to(medias, {
+        maskImage: "linear-gradient(transparent -25%, #000 0%, #000 100%)",
+        ease: "power3.inOut",
+        stagger: 0.5,
+        scrollTrigger: {
+          trigger: pinHeight,
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+          markers: true,
+        },
+      });
+
+
+      const mediasBackground = document.querySelectorAll(".slide-bg-img");
+      gsap.to(mediasBackground, {
+        maskImage: "linear-gradient(transparent -25%, #000 0%, #000 100%)",
+        ease: "power3.inOut",
+        stagger: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: pinHeight,
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+          markers: true,
+        },
+      });
+
+  });
+
+  return (
+    <div
+      className="pin-height"
+      style={{ height: `${(images?.length || 0) * 100}vh` }}
+    >
+      <div className="slider h-screen w-full">
+        {images?.map((image) => (
+          <div className="slide relative" key={image.url}>
+            <div className="slide-bg-img fixed top-0 left-0 w-full h-full z-10"
+             style={{
+              maskImage:
+                "linear-gradient(transparent 100%, #000 125%, #000 225%)",
+              WebkitMaskImage:
+                "linear-gradient(transparent 100%, #000 125%, #000 225%)",
+            }}>
+              <img
+                src={image.url}
+                alt={image.title}
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div
+              className="slide-main-img w-[30vw] aspect-square fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 overflow-hidden rounded-[0.8vw] z-20"
+              style={{
+                maskImage:
+                  "linear-gradient(transparent 100%, #000 125%, #000 225%)",
+                WebkitMaskImage:
+                  "linear-gradient(transparent 100%, #000 125%, #000 225%)",
+              }}
+            >
+              <div className="w-full h-full">
+                <img
+                  src={image.url}
+                  alt={image.title}
+                  className="media w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
